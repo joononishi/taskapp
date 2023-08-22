6ae69855cfea92724a3aea4cbee48df21b2595c4
Joe Onishi <joeoonishi@gmail.com>
2023-08-23 06:54:28 +0900

    .gitignoreファイルのみコミットでいいのでしょうか？

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import RealmSwift   //
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    // Realmインスタンスを取得する
+        let realm = try! Realm()  // ←追加
+    // DB内のタスクが格納されるリスト。
+        // 日付の近い順でソート：昇順
+        // 以降内容をアップデートするとリスト内は自動的に更新される。
+        var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)  // ←追加
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,13 +27,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
        // データの数（＝セルの数）を返すメソッド
        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 0
+           return taskArray.count  // ←修正する
+
        }
 
        // 各セルの内容を返すメソッド
        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
            // 再利用可能な cell を得る
            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+           // Cellに値を設定する  --- ここから ---
+            let task = taskArray[indexPath.row]
+            cell.textLabel?.text = task.title
+
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+            let dateString:String = formatter.string(from: task.date)
+            cell.detailTextLabel?.text = dateString
+                  // --- ここまで追加 ---
 
            return cell
        }
@@ -47,7 +66,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
        //UITableViewDataSourceプロトコルのメソッドで、Deleteボタンが押されたときにローカル通知をキャンセルし、データベースからタスクを削除する.
        //後ほどデータベースの実装を行なう際に、ここに追記するのでまずは空のままにしておきます。
        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-       }
-       
-   }
-
+           if editingStyle == .delete {
+                      // データベースから削除する
+                      try! realm.write {
+                          self.realm.delete(self.taskArray[indexPath.row])
+                          tableView.deleteRows(at: [indexPath], with: .fade)
+                      }
+       }// --- ここまで追加 ---
+    }
+}
